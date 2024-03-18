08fe0cf2ba091e7b52181d25fba6119ccbbf489b
jjan0516 <jjan05164354@gmail.com>
2024-03-19 00:54:28 +0900

    2024-03-19 update

---
+++ app/list/page.js
@@ -0,0 +1,14 @@
+export default function List() {
+  let 상품 = ["Tomatoes", "Pasta", "Coconut"];
+
+  return (
+    <div>
+      <h4 className="title">상품목록</h4>
+      {상품.map((a, i) => (
+        <div className="food">
+          <h4>{a} $40</h4>
+        </div>
+      ))}
+    </div>
+  );
+}
